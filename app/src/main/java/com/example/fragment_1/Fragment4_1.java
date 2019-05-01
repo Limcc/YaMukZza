080fe4716257f703a88c18a40c5722b332f2318e
hyun <tyel2910@naver.com>
2019-05-01 17:18:25 +0900

    tt

---
+++ app/src/main/java/com/example/fragment_1/Fragment4_1.java
@@ -0,0 +1,23 @@
+package com.example.fragment_1;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment4_1 extends Fragment {
+
+    public static Fragment4_1 newnstance(){
+        return new Fragment4_1();
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment4, container, false);
+    }
+
+}
