36729ca2926b5e23f9e34926e6bfce48fc4e683d
Limcc <ghostar98@gmail.com>
2019-05-11 20:29:11 +0900

    string 변화

--- app/src/main/java/com/example/fragment_1/Board.java
+++ app/src/main/java/com/example/fragment_1/Board.java
@@ -1,20 +1,45 @@
 package com.example.fragment_1;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class Board extends Fragment {
     public static Board newnstance(){
         return new Board();
     }
+    MainActivity activity;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        activity = (MainActivity) getActivity();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        activity = null;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        ViewGroup rootview = (ViewGroup) inflater.inflate(R.layout.board,container,false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.board, container, false);
+        Button button1 = (Button) rootview.findViewById(R.id.write);
+        button1.setOnClickListener(new View.OnClickListener() {
+            // 요청을 보내야 하는데 메인 액티비티에 다가 메소드를 하나 만들어야 한다.
+            @Override
+            public void onClick(View v) {
+                activity.replaceFragment(RecipeWrite.newnstance());
+            }
+        });
+        return rootview;
     }
 
 }
